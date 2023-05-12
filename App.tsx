697bf15122ffcb43c922a71d6e0a0e188656e8ff
Alysson Barrera <alyssonbarrera@outlook.com>
2023-05-12 01:14:42 -0300

    ✨ add realmdb and configure login

--- App.tsx
+++ App.tsx
@@ -1,15 +1,18 @@
+// eslint-disable-next-line camelcase
 import theme from '@theme/index'
 import {
   useFonts,
   Roboto_400Regular,
-  // eslint-disable-next-line camelcase
   Roboto_700Bold,
 } from '@expo-google-fonts/roboto'
+import { REALM_APP_ID } from '@env'
 import { StatusBar } from 'react-native'
+import { ThemeProvider } from 'styled-components/native'
+import { AppProvider, UserProvider } from '@realm/react'
 
+import { Home } from '@screens/Home'
 import { SignIn } from '@screens/SignIn'
 import { Loading } from '@components/Loading'
-import { ThemeProvider } from 'styled-components/native'
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
@@ -19,13 +22,17 @@ export default function App() {
   }
 
   return (
-    <ThemeProvider theme={theme}>
-      <StatusBar
-        barStyle="light-content"
-        backgroundColor="transparent"
-        translucent
-      />
-      <SignIn />
-    </ThemeProvider>
+    <AppProvider id={REALM_APP_ID}>
+      <ThemeProvider theme={theme}>
+        <StatusBar
+          barStyle="light-content"
+          backgroundColor="transparent"
+          translucent
+        />
+        <UserProvider fallback={SignIn}>
+          <Home />
+        </UserProvider>
+      </ThemeProvider>
+    </AppProvider>
   )
 }
--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -2,6 +2,7 @@
 import { Alert } from 'react-native'
 import { useEffect, useState } from 'react'
 
+import { Realm, useApp } from '@realm/react'
 import * as WebBrowser from 'expo-web-browser'
 import * as Google from 'expo-auth-session/providers/google'
 
@@ -16,6 +17,9 @@ WebBrowser.maybeCompleteAuthSession()
 
 export function SignIn() {
   const [isAuthenticating, setIsAuthenticating] = useState(false)
+
+  const app = useApp()
+
   const [_, response, googleSignIn] = Google.useAuthRequest({
     androidClientId: ANDROID_CLIENT_ID,
     iosClientId: IOS_CLIENT_ID,
@@ -34,11 +38,18 @@ export function SignIn() {
   useEffect(() => {
     if (response?.type === 'success') {
       if (response.authentication?.idToken) {
-        fetch(
-          `https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=${response.authentication.idToken}`,
+        const credentials = Realm.Credentials.jwt(
+          response.authentication.idToken,
         )
-          .then((response) => response.json())
-          .then(console.log)
+
+        app.logIn(credentials).catch((error) => {
+          console.log(error)
+          Alert.alert(
+            'Entrar',
+            'Não foi possível conectar-se a sua conta Google',
+          )
+          setIsAuthenticating(false)
+        })
       } else {
         Alert.alert('Entrar', 'Não foi possível conectar-se a sua conta Google')
         setIsAuthenticating(false)
